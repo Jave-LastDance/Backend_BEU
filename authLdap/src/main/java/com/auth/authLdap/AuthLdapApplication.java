bfc34a645dc6cdd3fa8c0f01639c7dd91a42015f
Alejo Diaz <alejandrodiazlote@gmail.com>
2023-09-11 14:31:07 -0500

    Conexion con ldap sin probar

---
+++ authLdap/src/main/java/com/auth/authLdap/AuthLdapApplication.java
@@ -0,0 +1,13 @@
+package com.auth.authLdap;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AuthLdapApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AuthLdapApplication.class, args);
+	}
+
+}
---
+++ authLdap/src/test/java/com/auth/authLdap/AuthLdapApplicationTests.java
@@ -0,0 +1,13 @@
+package com.auth.authLdap;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AuthLdapApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
